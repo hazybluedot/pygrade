7d83e898c6b53139f4009ca4f58c0d8dcaa2ef20
Darren Maczka <dmaczka@vt.edu>
2012-10-01 18:37:50 -0400

    cleaned up the interface, but still needs more

--- extract_url.py
+++ extract_url.py
@@ -9,10 +9,10 @@ import re
 from itertools import imap 
 
 url_regex = re.compile(r'(?P<url>https?://[^\s]+)')
-url_to_branch = re.compile(r'https?://github.com/([^\s/]+)/([^\s/]+)/tree/([^\s/]+)')
 url_with_sub = re.compile(r'https?://github.com/(?P<user>[^\s/]+)/(?P<repo>[^\s/]+)/tree/(?P<branch>[^\s/]+)/(?P<path>[^\s]+)')
+url_to_branch = re.compile(r'https?://github.com/([^\s/]+)/([^\s/]+)/tree/([^\s/]+)')
 
-def parse_url(match):
+def parse_url(match, line):
     url = match.group("url")
     (pre, url, directory) = line.partition(url)
     return (url, directory)
@@ -24,6 +24,20 @@ def parse_url_with_sub(match):
     directory = match.group("path")
     return ("https://github.com/{}/{}.git".format(user,repo), directory)
 
+def url_iterator(iterable):
+    for line in iterable:
+        match = url_regex.search(line)
+        if match:
+            url = match.group("url")
+            match_sub = url_with_sub.search(url)
+            match_branch = url_to_branch.search(url)
+            if match_sub:
+                yield parse_url_with_sub(match_sub)
+            else:
+                yield parse_url(match,line)
+        #elif match_branch:
+         #   sys.stderr.write("URL to Branch not implemented: {}\n".format(line))
+
 for (pid, file_path) in imap(shlex.split, sys.stdin):
     file_path = os.path.realpath(file_path)
     
@@ -33,38 +47,15 @@ for (pid, file_path) in imap(shlex.split, sys.stdin):
     lines = submission_text.splitlines()
 
     found_match = False
-    urls = {}
-    for line in lines:
         #not sure why pandoc would insert a literal non-breaking space in 'plain' output, but whatevz
-        line = line.replace('\302\240', r' ')
-        match = url_regex.search(line)
-        match2 = url_with_sub.search(line)
-        match_branch = url_to_branch.search(line)
-
-        if match:
-            (url, directory) = parse_url(match)
-        elif match2:
-            (url, directory) = parse_url_with_sub(match2)
-        elif match_branch:
-            sys.stderr.write("URL to Branch not implemented: {}\n".format(line))
-
-        if match or match2:
-            print "{} \"{}\" \"{}\"".format(pid,url,directory.strip(' ()[]{}/'))
-            try:
-                values = shlex.split(line)
-            except ValueError:
-                sys.stderr.write("{}: Unable to parse text: length {}\n".format(pid))
-            else:
-                urls[pid] = values[0]
-                found_match = True
-                try:
-                    path = values[1]
-                except IndexError:
-                    path = "."
-            break
+    #for url in ( match.group("url") for match in url_regex.search(imap(lambda x: x.replace('\302\240', ' '), lines)) if match ):
+    for (url, directory) in url_iterator( line for line in imap(lambda x: x.replace('\302\240', ' '), lines)):
+        found_match = True
+        print "{} \"{}\" \"{}\"".format(pid,url,directory.strip(' ()[]{}/'))
+ 
     if not found_match:
         sys.stderr.write("{}: No match found\n".format(pid))
-        if len(submission_text) > 1:
+        if len(submission_text) > 1 and len(submission_text) < 100:
             sys.stderr.writelines(submission_text)
 
 
--- gitpull.py
+++ gitpull.py
@@ -6,6 +6,7 @@ import os
 import shlex
 from github import Github #easy_install PyGitHub
 from git import * # aur/gitpython on Arch
+from itertools import imap
 
 github = Github()
 
@@ -63,6 +64,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Checkout/pull a list of git repositories from github.')
     parser.add_argument("-p","--pretend", action="store_true", help="Do everything except pull/clone repositories")
+    parser.add_argument("-r","--raw", action="store_true", help="Read input as raw git repositories to sync")
     parser.add_argument("basepath", nargs=1, help="Base path")
 
     args = parser.parse_args()
@@ -73,7 +75,18 @@ if __name__ == '__main__':
         sys.stderr.write("{}: not a real directory\n".format(basepath))
         sys.exit(1)
 
-    for (pid,url,project_directory) in map(shlex.split, sys.stdin):
-        repopath = sync_repo(pid,url,basepath,pretend=args.pretend)
-        project_directory = os.path.join(repopath,project_directory)
-        print "{} \"{}\"".format(pid,os.path.realpath(project_directory))
+    if args.raw:
+        for repodir in imap(str.strip, sys.stdin):
+            (repodir,git) = os.path.split(repodir)
+            repo = Repo(repodir)
+            origin = repo.remotes.origin
+            sys.stderr.write("Pulling from {}\n".format(repodir))
+            try:
+                origin.pull()
+            except AssertionError as e:
+                sys.stderr.write("{}: AssertionError\n".format(repodir))
+    else:
+        for (pid,url,project_directory) in imap(shlex.split, sys.stdin):
+            repopath = sync_repo(pid,url,basepath,pretend=args.pretend)
+            project_directory = os.path.join(repopath,project_directory)
+            print "{} \"{}\"".format(pid,os.path.realpath(project_directory))
