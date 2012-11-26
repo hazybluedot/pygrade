ea98f228b4e59d5dab14a1aa886bc6690f2fb988
Darren Maczka <dmaczka@vt.edu>
2012-11-26 11:59:15 -0500

    more lenient checking of my 'finished' comment

--- find_ungraded.sh
+++ find_ungraded.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find "$1" -name "$2" -print0 | xargs -0 grep -L '###DKM' | sed -r 's/^(.*)$/"\1"/g'
+find "$1" -name "$2" -print0 | xargs -0 grep -L '### *DKM' | sed -r 's/^(.*)$/"\1"/g'
