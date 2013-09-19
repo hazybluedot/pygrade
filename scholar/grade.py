990ed324fcccf03dff77338e3f39a1867afb77db
Darren Maczka <dmaczka@vt.edu>
2013-09-19 19:57:39 -0400

    refactor to make scholar a module directory, and update grade stuff to work with bare gradebook entry

---
+++ scholar/grade.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import csv 
+from errors import *
+
+default_args={ 'idfield': 'Student Id', 'headerlines': 0, 'gradefield': 'iotest part 1' }
+
+def open_grades(name, mode, margs=default_args):
+   mmargs=default_args
+   if 'name' in margs:
+       mmargs['gradefield'] = margs['name']
+
+   class ScholarGrades:
+      def __init__(self, f, mode):
+         self.file = f
+         self.idfield = mmargs['idfield']
+         self.gradefield = mmargs['gradefield']
+         self.mode = mode
+         self.entries = []
+         if hasattr(self.file, 'read'):
+            self.isflo = True
+         else:
+            #try:
+             self.isflo = False
+             self.file = open(f, 'r')
+         #else:
+         #   raise ScholarError("{0}: Must supply a file-like-object or filename to a Scholar grades.csv file".format(f))
+         
+         self.__read_entries(self.file)
+
+      def __enter__(self):
+         return self
+
+      def set(self, pid, grade):
+         try:
+            grade = float(grade)
+            if grade < 0: 
+               raise GradeError("Grade must be positive")
+
+         except ValueError as e:
+            raise GradeError("Grade must be a number")
+         try:
+            record = [ record for record in self.entries if record[self.idfield] == pid ][0]
+            record[self.gradefield] = grade
+         except IndexError as e:
+            raise ScholarError("{}: no such PID".format(pid))
+
+      def __read_entries(self, f):
+         sys.stderr.write("Reading entries from {}\n".format(self.file.name))
+         self.head=[f.next() for x in xrange(mmargs['headerlines'])]
+         reader = csv.DictReader(f)
+         self.fields = reader.fieldnames
+         if not (self.gradefield in self.fields and self.idfield in self.fields):
+            raise ScholarError("{0}: either no '{1}' or '{3}' in fields {2}".format(self.file.name, self.idfield, self.fields, self.gradefield))
+
+         def strip_dict(mydict):
+             try:
+                 return dict((key.strip(), val.strip()) for (key,val) in mydict.items())
+             except AttributeError as e:
+                 sys.stderr.write("{0}: {1}\n".format(e,mydict.items()))
+                 raise e
+
+         #for entry in reader:
+         #    sys.stderr.write("entry: {0}\n".format(strip_dict(entry)))
+         self.entries = [ entry for entry in reader ]#[ strip_dict(entry) for entry in reader ]
+
+      def student_list(self):
+         return dict([ (entry[self.idfield], 
+                          dict([('First Name', entry['First Name']), ('Last Name', entry['Last Name']) ]))
+                          for entry in self.entries ] )
+      
+      def pid_list(self):
+         return [ entry['PID'] for entry in self.entries ][:]
+
+      def write(self):
+         self.__write(self.file.name)
+
+      def __write(self, flo):
+         if mode == 'w':
+            writer = csv.DictWriter(flo,self.fields)
+            try:
+               flo.writelines(self.head)
+               writer.writeheader()
+               writer.writerows(self.entries)
+            except IOError as e:
+               raise GradeError("{}: {}".format(flo.name, e))
+
+      def close(self):
+         self.file.close()
+
+      def __exit__(self,type,value,traceback):
+         if self.mode == 'w':
+            if self.isflo:
+               self.write()
+               self.close()
+            else:
+               self.__write(sys.stdout)
+               #with open(self.file.name, 'wb') as flow:
+               #   self.__write(flow)
+         
+   return ScholarGrades(name, mode)
--- update_grades.py
+++ update_grades.py
@@ -2,17 +2,18 @@
 
 import argparse
 import sys
-import scholar
+from scholar import grade
 from itertools import imap
+from scholar.errors import *
 
 def update_grades(grades, fin):
     try:
         for (pid,grade) in imap(str.split, fin):
             try:
                 grades.set(pid, grade)
-            except scholar.ScholarError as e:
+            except ScholarError as e:
                 sys.stderr.write("ERROR: {} {}, {}\n".format(pid,grade,e))
-            except scholar.GradeError as e:
+            except GradeError as e:
                 sys.stderr.write("ERROR: {} {}, {}\n".format(pid,grade,e))
     except ValueError as e:
         sys.stderr.write("Input format should be 'PID GRADE' per line\n")
@@ -25,16 +26,21 @@ if __name__ == '__main__':
 
     parser = ap.ArgumentParser(description="Update Scholar grade file")
     parser.add_argument('-v', '--verbose',action='store_true',help="Be verbose")
+    parser.add_argument('-n', '--name', help="Name of Gradebook Item")
     parser.add_argument('file', nargs=1, help=("Grade file"))
     args = parser.parse_args()
 
     ## did we get a scholar archive, or a path to a grades.csv file?
-    try:
-        sc = scholar.ScholarArchive(args.file[0], 'w')
-        with sc.grades('w') as grades:
-            update_grades(grades, sys.stdin) ## doesn't work, can't write to file
-    except scholar.ScholarError as e:
+    #try:
+    #    sc = scholar.ScholarArchive(args.file[0], 'w')
+    #    with sc.grades('w') as grades:
+    #        update_grades(grades, sys.stdin) ## doesn't work, can't write to file
+    #except scholar.ScholarError as e:
         ## Not a Scholar Archive
-        with scholar.open_grades(args.file[0], 'w') as grades:
-            update_grades(grades, sys.stdin) ## this works
+    margs = {}
+    if args.name:
+        margs['name'] = args.name
+
+    with grade.open_grades(args.file[0], 'w', margs) as grades:
+        update_grades(grades, sys.stdin) ## this works
 
